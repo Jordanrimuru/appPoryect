41c24f06c418ea5d99f732831e938d7ef39bd73e
Jordanrimuru <Jordanrimuru@gmail.com>
2025-05-05 17:07:34 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Hola mis perrores js")
+});
+
+const PORT= 3000;
+app.listen(PORT,()=>{
+    console.log("Servidor escuchando localhost:3000")
+});
