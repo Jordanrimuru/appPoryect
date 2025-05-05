cf11cd9348868b8162a2a9a7948f81ada544d909
Jordanrimuru <Jordanrimuru@gmail.com>
2025-05-05 17:10:45 -0500

    elpepe

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("Hola mis perrores js")
+    res.send("Hola mis pñklmñomerrores")
 });
 
 const PORT= 3000;
